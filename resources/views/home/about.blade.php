01ace5b8b777e816839d5f020bf2584ead3768bc
Ben <git@othyn.com>
2019-07-09 19:30:33 +0100

    New about block implementation

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-sm">
+        <p>I've always been interested in technology, from my first foray with a computer.</p>
+
+        <p>Since I could code, I have.</p>
+
+        <p>Starting as an intern in software development at the beginning of 2014, I demonstrated my intrinsic passion for any and all things computing, which started from an early age, a passion that has flourished through supporting Open Source projects and beginning my career.</p>
+
+        <p>Within a short time, I was promoted from intern to Web Developer, and then promoted further into my current position, Senior Software Developer. I train, lead and support the next generation of excited Junior Developers, driving my ambition through their passion. I lead, manage and develop a rapidly expanding, award-winning product, delivering the best result for both business and stakeholders.</p>
+
+        <p>Software development is my calling, facilitated by my innate ability to quickly consume, understand and distil information to support others. I also have a strong sense of self motivation and passion for high-quality software development, always delivering to a progressive personal standard.</p>
+
+        <p>I am devoted. I live and breathe any and all things software and hardware and am pretty much interested in anything that has a plug socket on it; opening it up, tinkering around and seeing how it all works. I love the way I can create and let my mind run wild through, what is usually, a rigid and unforgiving electrical world - but I seem to have a knack for it, I speak its language.</p>
+    </div>
+</div>
