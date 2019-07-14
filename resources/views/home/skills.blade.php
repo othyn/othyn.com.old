245fea9b2e88596b3e4a437bc9519b96574b0bb4
Ben <git@othyn.com>
2019-07-14 23:42:22 +0100

    Flesh out content a little to see what the site looks like

--- resources/views/home/skills.blade.php
+++ resources/views/home/skills.blade.php
@@ -1,6 +1,39 @@
-{{-- Skill rankings for technologies/languages in HTOP style (progress bars, etc.) --}}
-<div class="row">
-    <div class="col-sm">
+<p>PHP 5-7 [Symfony, Laravel, CodeIgniter, PHPUnit, Composer]</p>
+<div class="progress-bar progress-bar-show-percent">
+    <div class="progress-bar-filled" style="width: 90%" data-filled="There's always room for learning."></div>
+</div>
 
-    </div>
+<p>JavaScript ES5-7 [Vue.js, Vuex, webpack]</p>
+<div class="progress-bar progress-bar-show-percent">
+    <div class="progress-bar-filled" style="width: 65%" data-filled="The rabbit hole is deep."></div>
+</div>
+
+<p>Swift 1-5 & SwiftUI</p>
+<div class="progress-bar progress-bar-show-percent">
+    <div class="progress-bar-filled" style="width: 45%" data-filled="UI Components ♥."></div>
+</div>
+
+<p>MySQL 5-8</p>
+<div class="progress-bar progress-bar-show-percent">
+    <div class="progress-bar-filled" style="width: 75%" data-filled="Big data. Big smarts."></div>
+</div>
+
+<p>HTML 5 & CSS 3 [SASS, LESS]</p>
+<div class="progress-bar progress-bar-show-percent">
+    <div class="progress-bar-filled" style="width: 95%" data-filled="The basics."></div>
+</div>
+
+<p>Tooling [ZSH, dotfiles, Git, Vagrant, Sublime Suite, Hyper, TablePlus]</p>
+<div class="progress-bar progress-bar-show-percent">
+    <div class="progress-bar-filled" style="width: 95%" data-filled="I know how I like it. Today."></div>
+</div>
+
+<p>AWS [EB, CD, EC2, S3, RDS] & DO</p>
+<div class="progress-bar progress-bar-show-percent">
+    <div class="progress-bar-filled" style="width: 75%" data-filled="Cloud. Big."></div>
+</div>
+
+<p>Computers [Hardware, Linux, macOS, Windows, network & systems admin]</p>
+<div class="progress-bar progress-bar-show-percent">
+    <div class="progress-bar-filled" style="width: 85%" data-filled="Anything with a plug."></div>
 </div>
