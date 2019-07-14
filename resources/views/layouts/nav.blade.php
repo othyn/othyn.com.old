1bd1b361a3e64f156a5862a8d4e52d7d0f100bb7
Ben <git@othyn.com>
2019-07-14 23:42:08 +0100

    Implement nav template

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,28 @@
+<div class="terminal-nav">
+    <div class="terminal-logo">
+        <div class="logo terminal-prompt">
+            <a href="/" class="no-style">
+                <typography-title is-full-screen></typography-title>
+            </a>
+        </div>
+    </div>
+    <nav class="terminal-menu">
+        <ul>
+            <li>
+                <a class="menu-item" href="#about">about.</a>
+            </li>
+            <li>
+                <a class="menu-item" href="#projects">projects.</a>
+            </li>
+            <li>
+                <a class="menu-item" href="#hobbies">hobbies.</a>
+            </li>
+            <li>
+                <a class="menu-item" href="#history">history.</a>
+            </li>
+            <li>
+                <a class="menu-item" href="#social">social.</a>
+            </li>
+        </ul>
+    </nav>
+</div>
