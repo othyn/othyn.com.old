233fdfe4e0e89f6e54d88175a0f576dcb1f9e06a
Ben <git@othyn.com>
2019-07-15 18:51:33 +0100

    Flesh out projects section

--- resources/views/home/projects.blade.php
+++ resources/views/home/projects.blade.php
@@ -1,7 +1,31 @@
 {{-- Git project listings, etc. --}}
 {{-- Cover dotfiles? --}}
-<div class="row">
-    <div class="col-sm">
+<div class="terminal-card-container">
+    <div class="terminal-card">
+        <header>char-count-es6</header>
+        <div>A very simple and light weight ES library for managing input lengths.</div>
+        <a href="https://github.com/othyn/char-count-es6" target="_blank" rel="noopener noreferrer" class="btn btn-block btn-ghost btn-default">view on github</a>
+    </div>
 
+    <div class="terminal-card">
+        <header>pub-golf</header>
+        <div>A pub golf web app to have some fun with. Made for a friends birthday!</div>
+        <a href="https://github.com/othyn/pub-golf" target="_blank" rel="noopener noreferrer" class="btn btn-block btn-ghost btn-default">view on github</a>
     </div>
 </div>
+
+<div class="terminal-card-container">
+    <div class="terminal-card">
+        <header>factorio-mods</header>
+        <div>Enjoy a spot of modding from time to time. Here are a few mods for the game factorio, which is a bloody good game.</div>
+        <a href="https://github.com/othyn?utf8=%E2%9C%93&tab=repositories&q=factorio" target="_blank" rel="noopener noreferrer" class="btn btn-block btn-ghost btn-default">view on github</a>
+    </div>
+
+    <div class="terminal-card">
+        <header>dotfiles</header>
+        <div>My dotfiles which follow me where ever I go! Stores my complete setup, with automated installation and management scripts.</div>
+        <a href="https://github.com/othyn/dotfiles" target="_blank" rel="noopener noreferrer" class="btn btn-block btn-ghost btn-default">view on github</a>
+    </div>
+</div>
+
+<a href="https://github.com/othyn" target="_blank" rel="noopener noreferrer" class="btn btn-block btn-primary">my github</a>
