581f47daa0f215768fe77c3fbe73a899ec44ea45
Ben <git@othyn.com>
2019-07-15 18:51:41 +0100

    Flesh out hobbies section

--- resources/views/home/hobbies.blade.php
+++ resources/views/home/hobbies.blade.php
@@ -1,6 +1,32 @@
 {{-- Talk a bit about photography, gaming, skiing, hiking? --}}
-<div class="row">
-    <div class="col-sm">
-
-    </div>
-</div>
+<table>
+    <thead>
+        <tr>
+            <th>Rank</th>
+            <th>Process</th>
+            <th>Descriptor</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>1</th>
+            <th>skiing</th>
+            <td>has to be skiing. Nothing like it, the experience. Recently, finally, invested in my own hear. Suited and booted.</td>
+        </tr>
+        <tr>
+            <th>2</th>
+            <th>hiking</th>
+            <td>shortly followed by hiking, which tends to preceed skiing. Getting into the outdoors, walking and experiencing new places. The views.</td>
+        </tr>
+        <tr>
+            <th>3</th>
+            <th>photography</th>
+            <td>which follows on nicely to photography. Capturing the moment and getting creative with the image, in the moment and in post is delightful. Fuji XT-2 crew.</td>
+        </tr>
+        <tr>
+            <th>4</th>
+            <th>motorsport</th>
+            <td>as a kid, I said i'd never do it. But now, I'm hooked. F1. A petrol head and electric head, combined brings a shine to Tesla. Although for now, Golf R 7.5 crew.</td>
+        </tr>
+    </tbody>
+</table>
