a247b448dae16cdec2550f22f8854c59d9e6907c
Ben <git@othyn.com>
2019-07-15 18:51:50 +0100

    Alter about section

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,10 +1,10 @@
-<p>I've always been interested in technology, from my first foray with a computer.</p>
+<p>Since I could code and tinker with electronics, I have.</p>
 
-<p>Since I could code, I have.</p>
+<p>Starting with getting usb modems working and operating system installs, to writing my first application in Visual Basic at around age 12, I have had an innate passion for all things tech.</p>
 
-<p>Starting as an intern in software development at the beginning of 2014, I demonstrated my intrinsic passion for any and all things computing, which started from an early age, a passion that has flourished through supporting Open Source projects and beginning my career.</p>
+<p>Through my teen years, I explored all aspects of computing, growing a keen knowledge of everything from power into the machine through to user interaction. Every step in the chain presenting a new avenue to explore and excite. Throughout that period, I explored software, looking at a wide range of languages - fiddling and building a solid knowledge of software design principals and taking part in the open source community. I also built computers. A lot of them - from budget builds to monster workstations. Later looking into virtualisation and home labbing.</p>
 
-<p>Within a short time, I was promoted from intern to Web Developer, and then promoted further into my current position, Senior Software Developer. I train, lead and support the next generation of excited Junior Developers, driving my ambition through their passion. I lead, manage and develop a rapidly expanding, award-winning product, delivering the best result for both business and stakeholders.</p>
+<p>Next came the beggings of my still early career, starting as an intern in software development at the beginning of 2014. Within a short time, I was promoted from intern to Web Developer, and then promoted further into my current position, Senior Software Developer. I train, lead and support the next generation of excited Junior Developers, driving my ambition through their passion. I lead, manage and develop a rapidly expanding, award-winning product, delivering the best result for both business and stakeholders.</p>
 
 <p>Software development is my calling, facilitated by my innate ability to quickly consume, understand and distil information to support others. I also have a strong sense of self motivation and passion for high-quality software development, always delivering to a progressive personal standard.</p>
 
