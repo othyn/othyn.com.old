c9a7b45a64438379f8c2516e43d5c095b0c5be7a
Ben <git@othyn.com>
2019-07-17 20:49:27 +0100

    Add social information, still deciding what to put on here

--- resources/views/home/social.blade.php
+++ resources/views/home/social.blade.php
@@ -1,15 +1,11 @@
-{{-- Social media and accounts page --}}
-<table>
-    <thead>
-        <tr>
-            <th>service</th>
-            <th>@me</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th>github</th>
-            <th><scramble>othyn</scramble></th>
-        </tr>
-    </tbody>
-</table>
+<p>In the rare occasion that you'd actually like to get in touch with me, ping me on one of these:</p>
+
+<div class="flex">
+    <!-- The goggles, they do nothing! -->
+    <p>github: <a href="https://github.com/othyn" target="_blank" rel="noopener noreferrer"><scramble>othyn</scramble></a></p>
+    <p>twitter: <a href="https://twitter.com/othyn_" target="_blank" rel="noopener noreferrer"><scramble>_othyn</scramble></a></p>
+    <p>email: <a href="mailto:hello@othyn.com" target="_blank" rel="noopener noreferrer"><scramble>hello@othyn.com</scramble></a></p>
+    <p>discord: <a href="https://discordapp.com" target="_blank" rel="noopener noreferrer"><scramble>othyn</scramble></a></p>
+    <p>steam: <a href="https://steamcommunity.com/id/Othyn" target="_blank" rel="noopener noreferrer"><scramble>othyn</scramble></a></p>
+    <p>xbox: <a href="https://xbox.com" target="_blank" rel="noopener noreferrer"><scramble>othyn</scramble></a></p>
+</div>
