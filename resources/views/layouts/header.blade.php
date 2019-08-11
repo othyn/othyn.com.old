e8927ea8bc06e4b6f6e9f605deeb8af1cbb1dcbc
Ben <git@othyn.com>
2019-08-11 20:02:20 +0100

    Default to usual site name if .env cache missmatches or doesn't update on deploy

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <title>{{ env('APP_NAME', '') }}. @yield('title').</title>
+    <title>{{ env('APP_NAME', 'othyn') }}. @yield('title').</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
