2510a8afc41aba3732ee8f8d1b6c03dcdfc8b17e
Ben <git@othyn.com>
2019-08-11 20:17:06 +0100

    Social tidy

--- resources/views/home/social.blade.php
+++ resources/views/home/social.blade.php
@@ -4,8 +4,6 @@
     <!-- The goggles, they do nothing! -->
     <p>github: <a href="https://github.com/othyn" target="_blank" rel="noopener noreferrer"><scramble>othyn</scramble></a></p>
     <p>twitter: <a href="https://twitter.com/othyn_" target="_blank" rel="noopener noreferrer"><scramble>_othyn</scramble></a></p>
-    <p>email: <a href="mailto:hello@othyn.com" target="_blank" rel="noopener noreferrer"><scramble>hello@othyn.com</scramble></a></p>
     <p>discord: <a href="https://discordapp.com" target="_blank" rel="noopener noreferrer"><scramble>othyn</scramble></a></p>
-    <p>steam: <a href="https://steamcommunity.com/id/Othyn" target="_blank" rel="noopener noreferrer"><scramble>othyn</scramble></a></p>
-    <p>xbox: <a href="https://xbox.com" target="_blank" rel="noopener noreferrer"><scramble>othyn</scramble></a></p>
+    <p>email: <a href="mailto:hello@othyn.com" target="_blank" rel="noopener noreferrer"><scramble>hello@othyn.com</scramble></a></p>
 </div>
