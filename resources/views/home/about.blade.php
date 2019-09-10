f695c8641adf4ca299c7630e7434ebd204db7f34
Ben <git@othyn.com>
2019-09-10 21:47:21 +0100

    Add some confetti!

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -6,7 +6,7 @@
 
 <p>Then my love of software began… I learnt a wide range of languages, fiddled and built a solid knowledge of software design principals and  participated in the open source community - and still do. I also built computers - a lot of them - from budget builds to monster workstations, while later discovering virtualisation and home-labbing.</p>
 
-<p>Next came the beginnings of my early career, starting as an intern in software development at the beginning of 2014. Within a short time, I was promoted from intern to Web Developer then promoted further into my current position, Senior Software Developer. I train, lead and support the next generation of excited Junior Developers, driving my ambition through their passion. I lead, manage and develop a rapidly expanding, award-winning product, delivering the best result for both business and stakeholders.</p>
+<p>Next came the beginnings of my early career, starting as an intern in software development at the beginning of 2014. Within a short time, I was <domfetti>promoted</domfetti> from intern to Web Developer then <domfetti>promoted</domfetti> further into my current position, Senior Software Developer. I train, lead and support the next generation of excited Junior Developers, driving my ambition through their passion. I lead, manage and develop a rapidly expanding, award-winning product, delivering the best result for both business and stakeholders.</p>
 
 <p>Software development is my calling, facilitated by my innate ability to quickly consume, understand and distil information to support others. I also have a strong sense of self-motivation and passion for high-quality software development, always delivering to a progressive personal standard.</p>
 
