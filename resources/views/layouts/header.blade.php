65bf8aecde9e7e9ac2bd1b91c5f642d3a84377f3
Ben <git@othyn.com>
2020-01-29 14:11:04 +0000

    FIX: Google Lighthouse Issue: SEO: Document does not have a meta description

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <meta name="description" content="Senior software engineer in the UK. This is my personal site.">
+
     {{-- <link rel="shortcut icon" href="/favicon.png"> --}}
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
