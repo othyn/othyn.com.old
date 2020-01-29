952673e12d889d1f48141e31c5e20dd902059f56
Ben <git@othyn.com>
2020-01-29 15:18:34 +0000

    Add favicon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,6 +7,10 @@
 
     <meta name="description" content="Senior software engineer in the UK. This is my personal site.">
 
-    {{-- <link rel="shortcut icon" href="/favicon.png"> --}}
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <link rel="preload" as="style" href="{{ mix('/css/app.css') }}">
 </head>
