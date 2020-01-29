7a7d8c12314117e8fc3722be4eb80d943d2ffcf4
Ben <git@othyn.com>
2020-01-29 16:35:21 +0000

    Bump version

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -1,1 +1,1 @@
-<p>Version: <a href="https://github.com/othyn/othyn.com/releases" target="_blank" rel="noopener noreferrer">1.2</a> <a class="pull-right" href="#app">Top</a></p>
+<p>Version: <a href="https://github.com/othyn/othyn.com/releases" target="_blank" rel="noopener noreferrer">1.3</a> <a class="pull-right" href="#app">Top</a></p>
