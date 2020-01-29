7fc67e7f5e8cf6d265d29ce8c245123dd3352d57
Ben <git@othyn.com>
2020-01-29 15:20:48 +0000

    Testing page paint speed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
-<script src="{{ mix('/js/manifest.js') }}"></script>
-<script src="{{ mix('/js/vendor.js') }}"></script>
-<script src="{{ mix('/js/app.js') }}"></script>
+<script src="{{ mix('/js/manifest.js') }}" async></script>
+<script src="{{ mix('/js/vendor.js') }}" async></script>
+<script src="{{ mix('/js/app.js') }}" async></script>
